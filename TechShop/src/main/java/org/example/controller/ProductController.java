d3ac3f2a90cc51ab4815c4c53bdede6980d0c46e
avsolon <avsolon@yandex.ru>
2025-03-24 19:02:03 +0700

    created product service, controller

--- TechShop/src/main/java/org/example/controller/ProductController.java
+++ TechShop/src/main/java/org/example/controller/ProductController.java
@@ -1,4 +1,77 @@
 package org.example.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.example.dto.ProductDTO;
+import org.example.model.Product;
+import org.example.service.ProductService;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/v1/products")
+@Tag(name="product")
+@Validated
+@AllArgsConstructor
 public class ProductController {
+
+    private ProductService productService;
+    private ModelMapper modelMapper;
+
+    @PostMapping
+    @Operation(summary = "Запрос на добавление товара")
+    public ResponseEntity<ProductDTO> addProduct(@Valid @RequestBody ProductDTO productDTO){
+        Product savedProduct = productService.addProduct(productDTO);
+        return new ResponseEntity<>(modelMapper.map(savedProduct, ProductDTO.class), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Запрос на удаление товара по id")
+    public ResponseEntity<Void> deleteProduct(@PathVariable UUID id){
+        productService.deleteProduct(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary = "Запрос на получение товара по id")
+    public ResponseEntity<ProductDTO> getProduct(@PathVariable UUID id){
+        Product product = productService.getProductById(id);
+        ProductDTO productDTO = modelMapper.map(product, ProductDTO.class);
+        return ResponseEntity.ok(productDTO);
+    }
+
+    @GetMapping
+    @Operation(summary = "Запрос на получение всех товаров")
+    public ResponseEntity<List<ProductDTO>> getAllProducts() {
+        List<Product> products = productService.getAllProducts();
+        List<ProductDTO> productDTOs = products.stream()
+                .map(product -> modelMapper.map(product, ProductDTO.class))
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(productDTOs);
+    }
+
+    @PatchMapping("/{id}/decrease-stock")
+    @Operation(summary = "Запрос на уменьшение товара")
+    public ResponseEntity<Void> decreaseStock(@PathVariable UUID id, @RequestParam Integer amount) {
+        try {
+            productService.decreaseStock(id, amount);
+            return ResponseEntity.noContent().build();
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(null);
+        }
+    }
+
+
 }
--- TechShop/src/main/java/org/example/service/ProductServiceImpl.java
+++ TechShop/src/main/java/org/example/service/ProductServiceImpl.java
@@ -1,9 +1,58 @@
 package org.example.service;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
+import org.example.dto.ProductDTO;
+import org.example.model.Product;
+import org.example.repository.ProductRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
 public class ProductServiceImpl implements ProductService{
+    private ProductRepository productRepository;
+    private SupplierService supplierService;
+    private ImageService imageService;
+
+    @Override
+    public Product addProduct(ProductDTO productDTO) {
+        Product product = Product.builder().
+                name(productDTO.getName()).
+                category(productDTO.getCategory()).
+                price(productDTO.getPrice()).
+                availableStock(productDTO.getAvailableStock()).
+                lastUpdateDate(productDTO.getLastUpdateDate()).build();
+        product.setSupplier(supplierService.getSupplierById(productDTO.getSupplierId()).get());
+        product.setImage(imageService.getImageById(productDTO.getImageId()).get());
+        return productRepository.save(product);
+    }
+
+    @Override
+    public void deleteProduct(UUID id) {
+        productRepository.deleteById(id);
+    }
+
+    @Override
+    public Product getProductById(UUID id) {
+        return productRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Product not found with id: " + id));
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public void decreaseStock(UUID id, Integer amount) {
+        Product product = getProductById(id);
+        if (product.getAvailableStock() < amount) {
+            throw new IllegalArgumentException("Not enough products" + amount);
+        }
+        product.setAvailableStock(product.getAvailableStock() - amount);
+        productRepository.save(product);
+    }
 }
